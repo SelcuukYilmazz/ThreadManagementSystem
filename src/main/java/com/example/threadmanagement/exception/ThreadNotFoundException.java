072c9cc1114cfe35cf608c9bc383b720dee935fd
SelcuukYilmazz <selcukyilmaz.dev@gmail.com>
2024-12-07 23:47:32 +0300

    Revert "Uygulama altyapısı oluşturuldu"
    This reverts commit 712bb3c0751b90f48c2081f4da927aebc4acf351.

--- src/main/java/com/example/threadmanagement/exception/ThreadNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.threadmanagement.exception;
-
-public class ThreadNotFoundException extends ThreadManagementException {
-    public ThreadNotFoundException(String threadId) {
-        super("Thread not found with ID: " + threadId);
-    }
-}
